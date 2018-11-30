59d45395242b98d2543dc2fcf09eb0c47b14ecfa
ftlka <smart.Julia@yandex.ru>
2018-11-30 13:14:06 +0500

    csscomb

--- client/src/components/payment/paymentContent/PaymentWindow1/CreditCard/CreditCard.js
+++ client/src/components/payment/paymentContent/PaymentWindow1/CreditCard/CreditCard.js
@@ -6,16 +6,16 @@ import pic from '../../../../../static/cards-logos.png';
 class CreditCard extends Component {
     render() {
         return (
-            <div className="CreditCard">
+            <div className="Credit-Card">
                 <img className="Cards-Logos" src={pic} alt="visa/mastercard logos" />
                 <form>
-                    <input type="text" className="Card-Number Card-Form"
+                    <input type="text" className="Card-Number Card-Form Form"
                         pattern="[0-9]{16}"
                         name="card-number" placeholder="Номер карты" required />
-                    <input type="text" className="Card-Exp Card-Form"
+                    <input type="text" className="Card-Exp Card-Form Form"
                         pattern="(0[1-9]|1[0-2])\/(1[8-9]|2[0-9]|3[0-7])"
                         name="card-expiration" placeholder="ММ/ГГ" required />
-                    <input type="text" className="Card-CVC Card-Form"
+                    <input type="text" className="Card-CVC Card-Form Form"
                         pattern="[0-9]{3}"
                         name="card-cvc" placeholder="CVC" required />
                 </form>
--- client/src/components/payment/paymentContent/PaymentWindow1/OtherPaymentInfo/OtherPaymentInfo.js
+++ client/src/components/payment/paymentContent/PaymentWindow1/OtherPaymentInfo/OtherPaymentInfo.js
@@ -3,12 +3,25 @@ import './OtherPaymentInfo.css';
 
 
 class OtherPaymentInfo extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="OtherPaymentInfo">
+                <div className="Detail Sum">
+                    <div className="Detail-Name Flex-Row-Elem">Сумма</div>
+                    <input className="Detail-Value Flex-Row-Elem Form" placeholder="от 1.000 до 75.000₽" required />
+                </div>
+                <div className="Detail Comment">
+                    <div className="Detail-Name Flex-Row-Elem">Комментарий</div>
+                    <input className="Detail-Value Flex-Row-Elem Form" placeholder="До 150 символов" />
+                </div>
+                <div className="Detail Mail">
+                    <div className="Detail-Name Flex-Row-Elem">Ваша эл.почта</div>
+                    <input className="Detail-Value Flex-Row-Elem Form" type="email"
+                        placeholder="Для квитанций об оплате" />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default OtherPaymentInfo;
